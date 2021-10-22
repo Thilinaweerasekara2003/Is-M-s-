4f25409ad328861729f02f4f655c6b37acf93528
ThilinaWeerasekara <84437905+Thilinaweerasekara2003@users.noreply.github.com>
2021-10-22 08:35:32 +0530

    Update gcast.py

--- ShadowMusic/modules/gcast.py
+++ ShadowMusic/modules/gcast.py
@@ -1,6 +1,5 @@
-# Shadow Music (Telegram bot project )
-# Copyright (C) 2021 Deshadeeth Thisarana
-
+# Isabella Music (Telegram bot project )
+# Copyright (C) 2021 Thilina Weerasekara
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
